bc24723bf1cf00ea64c091373f8e3101f2eac02a
diegodevs <diegom.develops@gmail.com>
2021-11-11 15:32:31 -0400

    [FIX] scroll of my orders

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -9,26 +9,33 @@ const MyOrder = () => {
 	const { state } = useContext(AppContext);
 	
 	return (
-		<aside className="MyOrder">
-			<div className="title-container">
-				<img src={arrow} alt="arrow" />
-				<p className="title">My order</p>
-			</div>
-			<div className="my-order-content">
-				{state.cart.map((product) => (
-					<OrderItem key={`orderItem-${product.id}` && console.log(product.id)} product={product} />
-				))}
-				<div className="order">
-					<p>
-						<span>Total</span>
-					</p>
-					<p>${useSumTotal(state.cart)}</p>
-				</div>
-				<button className="primary-button">
-					Checkout
-				</button>
-			</div>
-		</aside>
+		
+				<aside className="MyOrder">
+					<div className="title-container">
+						<img src={arrow} alt="arrow" />
+						<p className="title">My order</p>
+					</div>
+					<div className="my-order-content">
+						<scroll-container>
+							<scroll-page>
+								{state.cart.map((product) => (
+									<OrderItem key={`orderItem-${product.id}` && console.log(product.id)} product={product} />
+								))}
+							</scroll-page>
+							<div className="order">
+								<p>
+									<span>Total</span>
+								</p>
+								<p>${useSumTotal(state.cart)}</p>
+							</div>
+							<button className="primary-button">
+								Checkout
+							</button>
+						</scroll-container>
+					</div>
+					
+				</aside>
+			
 	);
 }
 
